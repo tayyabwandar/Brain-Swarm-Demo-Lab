9405a570ea2a9e5ad2fb3329e8d6c51408d06080
Muhammad Tayyab <tayyabwander420@gmail.com>
2026-07-29 20:00:13 +0500

    Refactor platformio.ini and main.cpp for Smart Environmental Monitoring and Control System: update library dependencies, improve OLED display integration, and enhance DHT sensor handling.

--- Week 4/Smart Environmental Monitoring and Control System/src/main.cpp
+++ Week 4/Smart Environmental Monitoring and Control System/src/main.cpp
@@ -1,119 +1,114 @@
 #include <Arduino.h>
-#include<Adafruit_SSD1306.h>
-#include<ESP32Servo.h>
-#include<Wire.h>
-#include<DHTesp.h>
+#include <Servo.h>
+#include <Adafruit_SH110X.h>
+#include <Wire.h>
+#include <DHT.h>
 
 
-#define DHTTYPE DHT22;
-#define DHTPIN 5;
-
-
-DHTesp dht;
+DHT dht(11,DHT22);
 Servo servo;
 
-Adafruit_SSD1306 display(128,64,&Wire,-1);
+Adafruit_SH1106G display(128, 64, &Wire, -1);
 
-ESP32PWM pwm;
 int freq = 1000;
 
- float temperature;
- float Humidity;
+float temperature = 0;
+float Humidity = 0;
 
-void oledSetup(){
-       display.begin(SSD1306_SWITCHCAPVCC,0x3C);
-        display.setTextSize(1);
+void oledSetup()
+{
+  display.begin(0x3C, true);
+  display.setTextSize(2);
 
-    display.setTextColor(SSD1306_WHITE);
-         display.print("Starting");
-         display.clearDisplay();
+  display.setTextColor(SH110X_WHITE);
+  display.print("Starting");
+  display.clearDisplay();
 };
-void servoSetup(){
-         pwm.attachPin(41,freq,12);
-
+void servoSetup()
+{
+  servo.attach(41);
+  servo.write(1024);
 };
-void dhtSetup(){
-              dht.setup(11,DHTesp::DHT22);
+void dhtSetup()
 
-}
-// void readDHT() {
-
-//     temperature = dht.getTemperature();
-//     delay(30);
-//     Humidity = dht.getHumidity();
-//     delay(30);
-
-// }
-
-void setup(){
-      Wire.begin(36,35);
-      pinMode(3,INPUT_PULLUP);
-      pinMode(6,INPUT);
-       Serial.begin(115200);
-       Serial.print("Smart Environmental Monitoring and Control System");
-       oledSetup();
-       servoSetup();
-       dhtSetup();
-
+{
+  dht.begin();
 }
 
-
-void oled(){
-  temperature = dht.getTemperature();
-    
-    Humidity = dht.getHumidity();
-    
-       display.clearDisplay();
-       display.setCursor(0,0);
-
-        if(isnan(temperature) || isnan(Humidity)){
-          display.print("Fail to get values");
-        }
-       Serial.print(dht.getStatusString());
-         
-         display.print("Temperature:");
-         display.println(temperature);
-         display.print("Humidity:");
-         display.print(Humidity);
-         delay(2000);
-         
-         if(temperature<25){
-          display.print("Cold");
-         }
-        else if(temperature==25){
-               display.print("Room Temperature");
-
-        }
-        else{
-          display.print("Hot");
-        }
-        display.display();
-
-
-}
-void servoFun(){
-        if(temperature<25){
-
-        servo.write(0);
-    
-        }
-        if(temperature>25)
-          servo.write(180);
+void setup()
+{
+  Wire.begin(36, 35);
+  pinMode(11,INPUT);
+  pinMode(6, INPUT);
+  Serial.begin(115200);
+  Serial.print("Smart Environmental Monitoring and Control System");
+  dhtSetup();
+  oledSetup();
+  servoSetup();
 }
 
-void Joystick(){
-    
-  int JoystickValuesX = analogRead(6);
-  int SEL = digitalRead(3);
-  int angle =map(JoystickValuesX,0,4095,0,180);
-  if(SEL==LOW){
-   servo.write(angle);
+void oled()
+{
+  temperature = dht.readTemperature();
+
+  Humidity = dht.readHumidity();
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  if (isnan(temperature) || isnan(Humidity))
+  {
+    display.print("Fail to get values");
+    Serial.print(Humidity);
   }
+  else
+  {
 
+    display.print("Temperature:");
+    display.println(temperature);
+    display.print("Humidity:");
+    display.print(Humidity);
+    delay(20);
+
+    if (temperature < 25)
+    {
+      display.print("Cold");
+    }
+    else if (temperature == 25)
+    {
+      display.print("Room Temperature");
+    }
+    else
+    {
+      display.print("Hot");
+    }
+    display.display();
+  }
 }
-void loop(){
-  // readDHT();
+
+void servoFun()
+{
+  int JoystickValuesX = analogRead(6);
+  int angle = map(JoystickValuesX, 0, 4095, 0, 180);
+  if(temperature!=25){
+    if (temperature < 25)
+  {
+
+    servo.write(0);
+  }
+  if (temperature > 25)
+    servo.write(180);
+  }
+  else{
+    servo.write(angle);
+  }
+  
+}
+
+
+
+void loop()
+{
   oled();
   servoFun();
-  Joystick();
 };
