481e1602367399af30d666849ee713655882b085
Muhammad Tayyab <tayyabwander420@gmail.com>
2026-08-19 22:06:47 +0500

    Update platformio.ini and main.cpp for LED control and sensor integration; add Wokwi configuration file

--- Week 7/Application Controlled LED/src/main.cpp
+++ Week 7/Application Controlled LED/src/main.cpp
@@ -1,18 +1,116 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "Smart University";
+const char* password = "";
+
+WebServer server(80);
+
+#define LED_PIN 2
+
+#define DHT_PIN 4
+#define DHT_TYPE DHT11
+
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+    Serial.begin(115200);
+    delay(1000);
+
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+
+    dht.begin();
+
+    
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    Serial.print("Connecting to WiFi");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.println("WiFi Connected!");
+
+    Serial.print("ESP32 IP Address: ");
+    Serial.println(WiFi.localIP());
+
+
+    
+    server.on("/led/on", HTTP_GET, []() {
+
+        digitalWrite(LED_PIN, HIGH);
+
+        server.send(
+            200,
+            "application/json",
+            "{\"success\":true,\"led\":true}"
+        );
+    });
+
+
+  
+    server.on("/led/off", HTTP_GET, []() {
+
+        digitalWrite(LED_PIN, LOW);
+
+        server.send(
+            200,
+            "application/json",
+            "{\"success\":true,\"led\":false}"
+        );
+    });
+
+
+  
+    server.on("/sensors", HTTP_GET, []() {
+
+        float temperature = dht.readTemperature();
+        float humidity = dht.readHumidity();
+
+        if (isnan(temperature) || isnan(humidity)) {
+
+            server.send(
+                500,
+                "application/json",
+                "{\"success\":false\"}"
+            );
+
+            return;
+        }
+
+        String json = "{";
+
+        json += "\"temperature\":";
+        json += String(temperature, 1);
+
+        json += ",\"humidity\":";
+        json += String(humidity, 1);
+
+        json += "}";
+
+        server.send(
+            200,
+            "application/json",
+            json
+        );
+    });
+
+
+    server.begin();
+
+    Serial.println("HTTP server started!");
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    server.handleClient();
 }
