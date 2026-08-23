099890ea10560001c866d10bd8a6c1e892674c50
Muhammad Tayyab <tayyabwander420@gmail.com>
2026-08-23 09:13:28 +0500

    Refactor PIR Motion-Triggered Smart Light project: update diagram.json, fix platformio.ini board name, enhance main.cpp logic for motion detection, and add wokwi.toml configuration file.

--- Week 6/PIR Motion-Triggered Smart Light/src/main.cpp
+++ Week 6/PIR Motion-Triggered Smart Light/src/main.cpp
@@ -1,18 +1,45 @@
-#include <Arduino.h>
+#include<Arduino.h>
+const int PIR_PIN = 2;
+const int LED_PIN = 1;
 
-// put function declarations here:
-int myFunction(int, int);
+unsigned long lastMotionTime = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  pinMode(PIR_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  
+  digitalWrite(LED_PIN, HIGH);
+
+  Serial.println("Smart Light Started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int motion = digitalRead(PIR_PIN);
+
+  
+  if (motion == HIGH) {
+
+    
+    digitalWrite(LED_PIN, LOW);
+
+    
+    lastMotionTime = millis();
+
+    Serial.println("Motion detected - Light ON");
+  }
+
+
+  if (millis() - lastMotionTime >= 10000) {
+
+    
+    digitalWrite(LED_PIN, HIGH);
+
+    Serial.println("No motion - Light OFF");
+  }
+
+  delay(50);
 }
