148a06a70cdba2210698c87b3488f0469aa772fe
Muhammad Tayyab <tayyabwander420@gmail.com>
2026-09-17 06:25:57 +0500

    Fix serial baud rate in main.cpp and update unwanted extensions in extensions.json; add video file

--- Week 7/Application Controlled LED/src/main.cpp
+++ Week 7/Application Controlled LED/src/main.cpp
@@ -17,7 +17,7 @@ DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
 
-    Serial.begin(115200);
+    Serial.begin(9600);
     delay(1000);
 
     pinMode(LED_PIN, OUTPUT);
@@ -28,6 +28,7 @@ void setup() {
     
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
+    
 
     Serial.print("Connecting to WiFi");
 
